3a0af56f9a54c4c1a5be04b7137246a6fa5ca2a5
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-26 12:33:33 +0000

    Get latest release

--- makerspace_run.sh
+++ makerspace_run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-wget https://github.com/CNC-Craft/makerspace-kinoite/releases/download/auto-iso/makerspace-kinoite-39-x86_64-20231220.iso -nc
+
+#Download latest ISO
+curl -s https://api.github.com/repos/CNC-Craft/makerspace-kinoite/releases/latest \
+| grep "browser_download_url.*iso\"" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -O makerspace-kinoite-39-x86_64.iso -qi -
+
 rm ./makerspace-kinoite-39-custom.iso
-./build_ks_iso.sh MakerspaceKinonite-ostree-x86_64 ./kickstart/ks.cfg ./makerspace-kinoite-39-x86_64-20231220.iso ./makerspace-kinoite-39-custom.iso
+./build_ks_iso.sh MakerspaceKinonite-ostree-x86_64 ./kickstart/ks.cfg ./makerspace-kinoite-39-x86_64.iso ./makerspace-kinoite-39-custom.iso
